e7d646f60624be03b9509f5b9dab46c14f3eb892
techraees <techraees786@gmail.com>
2026-01-16 16:17:30 +0500

    Add selection sort and bubble sort algorithms; update package dependencies

---
+++ sortingAlgorithms.js
@@ -0,0 +1,40 @@
+/**
+ * Selection Sort Algorithm
+ * 
+ * Select Iterated Element of first loop and and compare it with other remaining element in second loop and get the smallest one and 
+ * replace or swap the smallest with the element of interated index 
+ */
+export const selectionSortAscending = (arr) => {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    // Selection of element one by one
+    let minValueIndex = i;
+
+    for (let j = i + 1; j < n; j++) {
+      // Comparison of selected ele to other elements
+      if (arr[j] < arr[minValueIndex]) {
+        minValueIndex = j;
+      }
+    }
+
+    let temp = arr[i];
+
+    arr[i] = arr[minValueIndex];
+    arr[minValueIndex] = temp;
+  }
+};
+
+/**
+ * Bubble Sort Algorithm
+ * 
+ * Repeatedly swapping adjacent elements if they are in wrong order.
+ * On First iteration largest element comes to end and on second the second largest element comes to the second and so on 
+ */
+export const bubbleSortAlgo = (arr) => {
+
+
+};
+
+
+console.log(object)
